fd5b446c9ba90326f57526d4c6dc511e5680f809
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2024-07-30 00:39:53 +0200

    jest test logout

---
+++ src/js/api/auth/logout.test.js
@@ -0,0 +1,19 @@
+import { logout } from "./logout";
+import { remove } from "../../storage";
+
+jest.mock("../../storage", () => ({
+  remove: jest.fn(),
+}));
+
+describe("Logout functionality", () => {
+  beforeEach(function () {
+    jest.clearAllMocks();
+  });
+
+  it("should clear the token from browser storage", () => {
+    logout();
+
+    expect(remove).toHaveBeenCalledWith("token");
+    expect(remove).toHaveBeenCalledWith("profile");
+  });
+});
